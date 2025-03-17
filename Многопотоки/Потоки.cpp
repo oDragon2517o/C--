6160f2d5e06df974f16f540506856f748fd90a51
v-evseev <evseev.vasya@yandex.ru>
2025-03-17 17:50:16 +0300

    Потоки

---
+++ Многопотоки/Потоки.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+// https://www.youtube.com/watch?v=NawpxG81RRk&list=PLQOaTSbfxUtAc_RpyDiWCHq0YTzLtVSD0?
+
+using namespace std;
+
+void DoWork()
+{
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::cout << "ID потока = " << this_thread::get_id() << "\tDoWork\t" << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(1000));
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    thread th(DoWork);
+
+    th.detach();
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        std::cout << "ID потока = " << this_thread::get_id() << "\tmain\t" << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+
+    return 0;
+}
